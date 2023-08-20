ad599f714c0b507fb8f16a9066d881b007c74f6a
BodduBhavani <123790553+BodduBhavani@users.noreply.github.com>
2023-08-20 18:47:40 +0530

    Add files via upload

---
+++ BFS.py
@@ -0,0 +1,43 @@
+from collections import defaultdict, deque
+
+def bfs(graph, start, goal):
+    visited = set()
+    queue = deque([(start, [start])])
+
+    while queue:
+        node, path = queue.popleft()
+
+        if node == goal:
+            return path
+
+        if node not in visited:
+            visited.add(node)
+            for neighbor in graph[node]:
+                queue.append((neighbor, path + [neighbor]))
+
+    return None
+
+# Input graph nodes and edges
+graph = defaultdict(list)
+num_nodes = int(input("Enter the number of nodes: "))
+num_edges = int(input("Enter the number of edges: "))
+for _ in range(num_edges):
+    u, v = input("Enter edge (u v): ").split()
+    graph[u].append(v)
+    graph[v].append(u)
+
+# Input nodes
+nodes = list(graph.keys())
+print("Available nodes:", nodes)
+
+start_node = input("Enter the start node: ")
+goal_node = input("Enter the goal node: ")
+
+if start_node not in nodes or goal_node not in nodes:
+    print("Invalid node entered.")
+else:
+    path = bfs(graph, start_node, goal_node)
+    if path:
+        print("Path from", start_node, "to", goal_node, ":", " -> ".join(path))
+    else:
+        print("No path found from", start_node, "to", goal_node)
