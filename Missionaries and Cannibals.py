7d3c780d31653418ead09ffaeac9d57a5470ca17
BodduBhavani <123790553+BodduBhavani@users.noreply.github.com>
2023-08-31 09:56:58 +0530

    Update Missionaries and Cannibals.py

--- Missionaries and Cannibals.py
+++ Missionaries and Cannibals.py
@@ -1,4 +1,5 @@
-def valid_state(m_left, c_left, m_right, c_right):
+def valid_state(state):
+    m_left, c_left, m_right, c_right, side = state
     if m_left < 0 or c_left < 0 or m_right < 0 or c_right < 0:
         return False
     if m_left > 0 and m_left < c_left:
@@ -7,28 +8,30 @@ def valid_state(m_left, c_left, m_right, c_right):
         return False
     return True
 
-def print_state(m_left, c_left, m_right, c_right):
-    print(f"Left side: {m_left}M {c_left}C | Right side: {m_right}M {c_right}C")
+def print_state(state):
+    m_left, c_left, m_right, c_right, side = state
+    print(f"Left side: {m_left}M {c_left}C | Right side: {m_right}M {c_right}C | Boat: {'Left' if side == 0 else 'Right'}")
 
-def solve(m_left, c_left, m_right, c_right):
+def solve(initial_state):
+    m_left, c_left, _, _, _ = initial_state
     print("Initial state:")
-    print_state(m_left, c_left, m_right, c_right)
+    print_state(initial_state)
     print("\n")
 
     visited_states = set()
-    stack = [(m_left, c_left, m_right, c_right, None)]
+    stack = [(*initial_state, None)]
 
     while stack:
-        m_left, c_left, m_right, c_right, prev_action = stack.pop()
+        m_left, c_left, m_right, c_right, side, prev_action = stack.pop()
 
-        if (m_left, c_left, m_right, c_right) in visited_states:
+        if (m_left, c_left, m_right, c_right, side) in visited_states:
             continue
         
-        visited_states.add((m_left, c_left, m_right, c_right))
+        visited_states.add((m_left, c_left, m_right, c_right, side))
 
         if m_left == 0 and c_left == 0:
             print("Goal state reached:")
-            print_state(m_left, c_left, m_right, c_right)
+            print_state((m_left, c_left, m_right, c_right, side))
             break
 
         for action in [(1, 0), (2, 0), (0, 1), (0, 2), (1, 1)]:
@@ -39,15 +42,23 @@ def solve(m_left, c_left, m_right, c_right):
             new_c_left = c_left - action[1]
             new_m_right = m_right + action[0]
             new_c_right = c_right + action[1]
+            new_side = 1 - side  # Toggle the side of the boat
+            new_state = (new_m_left, new_c_left, new_m_right, new_c_right, new_side)
 
-            if valid_state(new_m_left, new_c_left, new_m_right, new_c_right):
-                print(f"Move: {action[0]}M {action[1]}C")
-                print_state(new_m_left, new_c_left, new_m_right, new_c_right)
-                stack.append((new_m_left, new_c_left, new_m_right, new_c_right, action))
+            if valid_state(new_state):
+                if side == 0 and new_side == 1:
+                    boat_direction = "from Left to Right"
+                else:
+                    boat_direction = "from Right to Left"
+                
+                print(f"Move Boat {boat_direction}: {action[0]}M {action[1]}C")
+                print_state(new_state)
+                stack.append((*new_state, action))
 
 # Take user inputs for the number of missionaries and cannibals
 m = int(input("Enter the number of missionaries: "))
 c = int(input("Enter the number of cannibals: "))
+initial_state = (m, c, 0, 0, 0)  # The boat starts on the left side
 
 # Initialize the problem with the user input values
-solve(m, c, 0, 0)
+solve(initial_state)
